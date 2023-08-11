6991ca4236ee8005190b2333cb0288ba7309925e
Edson Martins <edsonmartins3110@gmail.com>
2023-08-11 19:14:02 -0300

    Implementar feign client para hr-worker

---
+++ hr-payroll/src/main/java/br/com/developerspills/hrpayroll/feignclients/WorkerFeignClient.java
@@ -0,0 +1,18 @@
+package br.com.developerspills.hrpayroll.feignclients;
+
+
+import br.com.developerspills.hrpayroll.dtos.Worker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
+
+@Component
+@FeignClient(name = "hr-worker", url = "localhost:8081", path = "/api/workers")
+public interface WorkerFeignClient {
+    @GetMapping("{uid}")
+    ResponseEntity<Worker> loadWorker(@PathVariable UUID uid);
+}
--- hr-payroll/src/main/java/br/com/developerspills/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/developerspills/hrpayroll/services/PaymentService.java
@@ -1,29 +1,19 @@
 package br.com.developerspills.hrpayroll.services;
-
-import br.com.developerspills.hrpayroll.dtos.Worker;
 import br.com.developerspills.hrpayroll.entities.Payment;
+import br.com.developerspills.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.UUID;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(UUID workerUid, int days){
-
-        var url = workerHost + "/api/workers/" + workerUid.toString();
-
-        var worker = restTemplate.getForObject(url, Worker.class);
-
+        var worker = workerFeignClient.loadWorker(workerUid).getBody();
         return new Payment(worker.name(), worker.dailyIncome(), days);
     }
 }
