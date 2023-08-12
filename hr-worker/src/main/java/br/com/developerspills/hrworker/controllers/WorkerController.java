57dbe4f3289eb968714737a80b777ccb994c530e
Edson Martins <edsonmartins3110@gmail.com>
2023-08-12 08:44:31 -0300

    comenta port das properties e adiciona logger

--- hr-worker/src/main/java/br/com/developerspills/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/br/com/developerspills/hrworker/controllers/WorkerController.java
@@ -2,7 +2,10 @@ package br.com.developerspills.hrworker.controllers;
 
 import br.com.developerspills.hrworker.dtos.WorkerResponse;
 import br.com.developerspills.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,12 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/workers")
 public class WorkerController {
+
+    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerRepository workerRepository;
 
@@ -26,6 +35,9 @@ public class WorkerController {
 
     @GetMapping("{uid}")
     public ResponseEntity<WorkerResponse> loadWorker(@PathVariable UUID uid){
+
+        logger.info("PORT = "  + env.getProperty("local.server.port"));
+
         var resultOptional = workerRepository.findById(uid);
 
         if(resultOptional.isEmpty()){
